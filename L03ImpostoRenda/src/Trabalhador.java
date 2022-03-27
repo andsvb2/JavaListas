cc2e988054c53b57540e1720e84f3912ddece692
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-03-27 15:26:10 -0300

    Cria classe Trabalhador.

---
+++ L03ImpostoRenda/src/Trabalhador.java
@@ -0,0 +1,24 @@
+
+public class Trabalhador {
+
+	private float salarioBruto;
+	private int dependentes;
+	
+	
+	public float getSalarioBruto() {
+		return salarioBruto;
+	}
+	
+	public void setSalarioBruto(float novoSalarioBruto) {
+		salarioBruto = novoSalarioBruto;
+	}
+	
+	public int getDependentes() {
+		return dependentes;
+	}
+	
+	public void setDependentes(int novoDependentes) {
+		dependentes = novoDependentes;
+	}
+	
+}
