9e93020989a674527e637a46be31c03ba472c4ba
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-03-29 12:10:32 -0300

    Elimina var, corrige input e reformata saída.

--- L03ImpostoRenda/src/ImpostoRecolhido.java
+++ L03ImpostoRenda/src/ImpostoRecolhido.java
@@ -6,29 +6,29 @@ public class ImpostoRecolhido {
 		
 		Contador c = new Contador();
 		Scanner input = new Scanner(System.in);
-		int resposta = 1, controle = 0;
+		int controle = 0;
 		float impostoRecolhido = 0;
 		
 		System.out.print("Quantidade de funcionários: ");
-		int funcionarios = Integer.parseInt(input.nextLine());
+		int funcionarios = input.nextInt();
 		System.out.println("");
 
 		do {
 			Trabalhador t = new Trabalhador();
 			
 			System.out.print("Salário bruto: ");
-			t.setSalarioBruto(Float.parseFloat(input.nextLine()));
+			t.setSalarioBruto(input.nextFloat());
 			
 			System.out.print("Dependentes: ");
-			t.setDependentes(Integer.parseInt(input.nextLine()));
+			t.setDependentes(input.nextInt());
 			
+			System.out.println("");
 			controle++;
 			impostoRecolhido += c.calcularIR(t);
 			
 		} while (controle < funcionarios);
 		input.close();
 		
-		System.out.printf("%nForam informados %d funcionários.%nO total de imposto pago pela empresa é de R$ %.2f.", controle, impostoRecolhido);
+		System.out.printf("O total de imposto pago pela empresa é de R$ %.2f.", impostoRecolhido);
 	}
-
 }
