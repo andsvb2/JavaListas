41236da4516cba6d1eaf72b8676d8c451d4ce763
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-04-04 15:37:46 -0300

    Cria programa gerador de triângulos, da lista 04.

---
+++ L04TrianguloFloyd/src/MainTriangulo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MainTriangulo {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Digite o número que forma o limite do triângulo: ");
+		int num = input.nextInt();
+		input.close();
+		
+		/*
+		 * O trecho abaixo foi adaptado de: https://stackoverflow.com/a/54758926/16978367
+		 */
+		
+		for(int i = 0, controle = 0; i < num; controle++) {
+			for(int j = 0; j < controle; i++) {
+				if (i == num) {
+					break;
+				} else {
+					System.out.print(i + 1 + " ");
+					j++;
+				}
+			}
+			System.out.println(" ");
+		}
+	}
+}
