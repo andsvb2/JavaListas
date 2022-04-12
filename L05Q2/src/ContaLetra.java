8f399d45fcf9a88451c670dfc086a38f4c9947aa
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-04-12 15:15:48 -0300

    Cria arquivos para questão 02, lista 05.

---
+++ L05Q2/src/ContaLetra.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ContaLetra {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		input.close();
+	}
+
+}
