bae54aa70df8742ab1ba19c000121c965a5f7574
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-04-22 10:13:44 -0300

    Commit para atualização de token no eclipse

--- L06ClientesBanco/src/Banco.java
+++ L06ClientesBanco/src/Banco.java
@@ -1,4 +1,40 @@
 
 public class Banco {
-
+	
+	private ContaBancaria[] clientes;
+	private int qtdClientes = 0;
+	
+	public Banco(int tamanhoArray) {
+		clientes = new ContaBancaria[tamanhoArray];
+	}
+	
+	public boolean adicionarConta(ContaBancaria novaConta) {
+		if (eRepetido(novaConta) && qtdClientes < (clientes.length - 1)) {
+			clientes[qtdClientes++] = novaConta;
+			return true;
+		}
+		return false;
+	}
+	
+	public ContaBancaria recuperarConta(String codAgencia, String numConta) {
+		
+		return null;
+	}
+	
+	public ContaBancaria[] getClientes() {
+		return clientes;
+	}
+	
+	public int getQtdClientes() {
+		return qtdClientes;
+	}
+	
+	private boolean eRepetido(ContaBancaria contaTestada) {
+		boolean flag = false;
+		for (ContaBancaria i: clientes) {
+			if ((i.equals(contaTestada) && i.getTitular().equals(contaTestada.getTitular())))
+				flag = true;
+		}
+		return flag;
+	}
 }
