cb3861d51015e4dc75b3b8eac00c92a836fea65d
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-04-24 15:56:14 -0300

    cria função b() e reorganiza o código

--- L07Operacoes/src/Programa.java
+++ L07Operacoes/src/Programa.java
@@ -3,16 +3,49 @@ public class Programa {
 
 	public static void main(String[] args) {
 
-		public void a(){
-			Operacoes op = new Operacoes();
-			int x = 2;
-			op.dobro(x);
-			System.out.println("Letra a: " + x);
-		}
-
-
-
-
+		a();
+		b();
+		c();
+		d();
+		e();
+		f();
+		g();
 	}
 
+	public static void a(){
+		Operacoes op = new Operacoes();
+		int x = 2;
+		op.dobro(x);
+		System.out.println("Letra a: " + x);
+	}
+	
+	public static void b() {
+		Operacoes op = new Operacoes();
+		System.out.println(op.dobro("ba"));;
+	}
+	
+	public static void c() {
+		Operacoes op = new Operacoes();
+		int x[] = {1, 3, 6, 9};
+		op.dobro(x);
+		for(int i : x) {
+			System.out.println(i);
+		}
+	}
+	
+	public static void d() {
+		
+	}
+	
+	public static void e() {
+		
+	}
+	
+	public static void f() {
+		
+	}
+	
+	public static void g() {
+		
+	}
 }
