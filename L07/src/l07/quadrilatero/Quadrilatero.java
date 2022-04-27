1c2bdc971f80c347b1bc225b4e5cc29ac420686d
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-04-27 11:52:35 -0300

    cria pacote l07.quadrilatero

---
+++ L07/src/l07/quadrilatero/Quadrilatero.java
@@ -0,0 +1,32 @@
+package l07.quadrilatero;
+
+public class Quadrilatero {
+	
+	private int altura;
+	private int comprimento;
+	
+	public Quadrilatero(int alt, int com) {
+		altura = alt;
+		comprimento = com;
+	}
+	
+	public Quadrilatero(int lado) {
+		altura = comprimento = lado;
+	}
+	
+	public boolean equals(Quadrilatero q) {
+		return (q.altura == this.altura && q.comprimento == this.comprimento);
+	}
+	
+	public boolean isQuadrado() {
+		return (altura == comprimento);
+	}
+	
+	public int getArea() {
+		return altura * comprimento;
+	}
+	
+	public int getPerimetro() {
+		return 2 * (altura + comprimento);
+	}
+}
